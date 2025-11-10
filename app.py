b44fd44221871ed124e71164dcd97b6a951249c5
Rodrigo Schiavinatto Plassmann <162203993+rodrigoplassmann@users.noreply.github.com>
2025-11-10 23:34:04 +0000

    Função com as métricas

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ import platform
 
 APP = Flask(__name__)
 
-@APP.get("/")
-def index():
+def metricas():
     nomes = "Rodrigo Schiavinatto Plassmann e Thomas Manussadjian Steinhausser"
     pid = os.getpid()
     memoria_mb = psutil.virtual_memory().used/1024 ** 2
     porcentagem_cpu = psutil.cpu_percent(interval=1)
     sistema_operacional = platform.platform()
 
-    return f"""
-        <h2>Nomes: {nomes}</h2>
-        <h2>PID: {pid}</h2>
-        <h2>Memória usada: {memoria_mb:.1f} MB</h2>
-        <h2>CPU: {porcentagem_cpu}%</h2>
-        <h2>Sistema operacional: {sistema_operacional}</h2>
-    """
+    return {
+        "nomes": nomes,
+        "pid": pid,
+        "memoria_mb": round(memoria_mb, 1),
+        "porcentagem_cpu": porcentagem_cpu,
+        "sistema_operacional": sistema_operacional
+    }
+
+@APP.get("/")
+def index():
+    
 
 if __name__ == "__main__":
     APP.run(host = "0.0.0.0", port = 5000)
