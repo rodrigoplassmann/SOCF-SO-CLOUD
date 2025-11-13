54d400d7916117d27fd72b02f49cc2a376a943e1
Rodrigo Schiavinatto Plassmann <162203993+rodrigoplassmann@users.noreply.github.com>
2025-11-13 14:12:36 +0000

    Arrumar variável porcentagem_cpu

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def coletar_metricas():
         "integrantes": INTEGRANTES,
         "pid": PID,
         "memoria_mb": round(memoria_mb, 1),
-        "cpu_percent": cpu_percent,
+        "porcentagem_cpu": porcentagem_cpu,
         "sistema_operacional": SISTEMA
     }
 
@@ -31,9 +31,9 @@ def index():
 
         <h3>Métricas atuais</h3>
         <p><b>Integrantes:</b> {m['integrantes']}</p>
-        <p><b>PID do processo:</b> {m['pid']}</p>
-        <p><b>Memória utilizada (MB):</b> {m['memoria_mb']}</p>
-        <p><b>Uso de CPU (%):</b> {m['cpu_percent']}</p>
+        <p><b>PID:</b> {m['pid']}</p>
+        <p><b>Memória usada:</b> {m['memoria_mb']} MB</p>
+        <p><b>Uso de CPU:</b> {m['porcentagem_cpu']}%</p>
         <p><b>Sistema operacional:</b> {m['sistema_operacional']}</p>
 
         <p><a href="/info"><button>Info (JSON)</button></a></p>
